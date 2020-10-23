704708010176de33090b4f6e51890d7b03277505
paprota55 <paprota55@gmail.com>
2020-10-23 19:16:09 +0200

    Margine changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { NgImageSliderModule, NgImageSliderComponent } from 'ng-image-slider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'coraggiomen-first';
+  title = 'Coraggio men';
+  siteName = 'https://www.coraggiomen.pl';
+  h1 = 'Sklep internetowy w trakcie realizacji.';
+  h2 = 'Nasze produkty';
+  h3 = 'Zapraszamy do kontaktu mailowego:';
+  mail = 'biuro@coraggiomen.pl';
+  imageObject: Array<object> = [{
+    image: 'assets/calosc.jpg',
+    thumbImage: 'assets/calosc.jpg',
+    alt: 'Image alt',
+    title: ''
+  }, {
+    image: 'assets/koszule.jpg',
+    thumbImage: 'assets/koszule.jpg',
+    title: '',
+    alt: 'Image alt'
+  }, {
+    image: 'assets/buty1.jpg',
+    thumbImage: 'assets/buty1.jpg',
+    title: '',
+    alt: 'Image alt'
+  }, {
+    image: 'assets/buty2.jpg',
+    thumbImage: 'assets/buty2.jpg',
+    title: '',
+    alt: 'Image alt'
+  }, {
+    image: 'assets/buty3.jpg',
+    thumbImage: 'assets/buty3.jpg',
+    title: '',
+    alt: 'Image alt'
+  }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgImageSliderModule,
     BrowserModule
   ],
   providers: [],
