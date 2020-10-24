cb631ec5a9e4b0670a3b874eb77ab3455d714474
paprota55 <paprota55@gmail.com>
2020-10-24 13:31:44 +0200

    Changed slider elements, added margin to right and left div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,34 +11,99 @@ export class AppComponent {
   title = 'Coraggio men';
   siteName = 'https://www.coraggiomen.pl';
   h1 = 'Sklep internetowy w trakcie realizacji.';
-  h2 = 'Nasze produkty';
+  h2 = 'Obecny katalog';
   h3 = 'Zapraszamy do kontaktu mailowego:';
   mail = 'biuro@coraggiomen.pl';
   imageObject: Array<object> = [{
-    image: 'assets/calosc.jpg',
-    thumbImage: 'assets/calosc.jpg',
-    alt: 'Image alt',
+    image: 'assets/katalog-02.png',
+    thumbImage: 'assets/katalog-02.png',
+    alt: 'start',
     title: ''
   }, {
-    image: 'assets/koszule.jpg',
-    thumbImage: 'assets/koszule.jpg',
+    image: 'assets/katalog-03.png',
+    thumbImage: 'assets/katalog-03.png',
     title: '',
-    alt: 'Image alt'
+    alt: 'paski'
   }, {
-    image: 'assets/buty1.jpg',
-    thumbImage: 'assets/buty1.jpg',
+    image: 'assets/katalog-04.png',
+    thumbImage: 'assets/katalog-04.png',
     title: '',
-    alt: 'Image alt'
+    alt: 'obuwie'
   }, {
-    image: 'assets/buty2.jpg',
-    thumbImage: 'assets/buty2.jpg',
+    image: 'assets/katalog-05.png',
+    thumbImage: 'assets/katalog-05.png',
     title: '',
-    alt: 'Image alt'
+    alt: 'obuwie'
   }, {
-    image: 'assets/buty3.jpg',
-    thumbImage: 'assets/buty3.jpg',
+    image: 'assets/katalog-06.png',
+    thumbImage: 'assets/katalog-06.png',
     title: '',
-    alt: 'Image alt'
+    alt: 'obuwie'
+  }, {
+    image: 'assets/katalog-06.png',
+    thumbImage: 'assets/katalog-06.png',
+    title: '',
+    alt: 'obuwie'
+  }, {
+    image: 'assets/katalog-07.png',
+    thumbImage: 'assets/katalog-07.png',
+    title: '',
+    alt: 'obuwie'
+  }, {
+    image: 'assets/katalog-08.png',
+    thumbImage: 'assets/katalog-08.png',
+    title: '',
+    alt: 'obuwie'
+  }, {
+    image: 'assets/katalog-09.png',
+    thumbImage: 'assets/katalog-09.png',
+    title: '',
+    alt: 'obuwie'
+  }, {
+    image: 'assets/katalog-10.png',
+    thumbImage: 'assets/katalog-10.png',
+    title: '',
+    alt: 'obuwie'
+  }, {
+    image: 'assets/katalog-11.png',
+    thumbImage: 'assets/katalog-11.png',
+    title: '',
+    alt: 'obuwie premium'
+  }, {
+    image: 'assets/katalog-12.png',
+    thumbImage: 'assets/katalog-12.png',
+    title: '',
+    alt: 'paski'
+  }, {
+    image: 'assets/katalog-13.png',
+    thumbImage: 'assets/katalog-06.png',
+    title: '',
+    alt: 'paski'
+  }, {
+    image: 'assets/katalog-13.png',
+    thumbImage: 'assets/katalog-06.png',
+    title: '',
+    alt: 'paski'
+  }, {
+    image: 'assets/katalog-14.png',
+    thumbImage: 'assets/katalog-14.png',
+    title: '',
+    alt: 'paski'
+  }, {
+    image: 'assets/katalog-15.png',
+    thumbImage: 'assets/katalog-15.png',
+    title: '',
+    alt: 'paski'
+  }, {
+    image: 'assets/katalog-16.png',
+    thumbImage: 'assets/katalog-16.png',
+    title: '',
+    alt: 'spodnie'
+  }, {
+    image: 'assets/katalog-17.png',
+    thumbImage: 'assets/katalog-17.png',
+    title: '',
+    alt: 'koszule'
   }
   ];
 
